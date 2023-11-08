7e586ca72034b2d1cb7c0e46daaf453391790055
Kamira2005 <119340467+Kamira2005@users.noreply.github.com>
2023-11-08 20:12:03 +0100

    Zmiany

--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -1,25 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
 
 public class player : MonoBehaviour
 {
     public float moveSpeed = 5f; // Adjust this value to control the movement speed.
     private Rigidbody2D rb;
+    public int a, b;
 
+    private int punkty=0;
+    public TextMeshProUGUI punktyTxt;
+    //private int wiśnie
     // Start is called before the first frame update
+
     void Start()
     {
         Witam();
 
          rb = GetComponent<Rigidbody2D>();
-         Debug.Log (Suma(2,3));
-        
+        Debug.Log (Suma(a,b)); 
+        punktyTxt.text=punkty.ToString();
     }
    
     // Update is called once per frame
     void Update()
     {
+       
+        
+        
         // Get input from the player
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
@@ -42,10 +52,28 @@ public class player : MonoBehaviour
     }
     //napisz publiczną metode typu int która przyjmuje parametry będące zmiennymi typu int A i B, oraz zwraca sume tych zmiennych
     //napisz metode która liczy sume A i B
-    public int Suma(int A, int B)
+    public int Suma(int a, int b)
     {
-        return A+B;
+        return a+b;
         
         
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("wisienka")) // Zderzenie z obiektem o tagu "ObiektDoZderzenia".
+        {
+            punkty++; // Zwiększ ilość punktów gracza o 1.
+            Debug.Log("Zdobyto punkt! Aktualna ilość punktów: " + punkty);
+            punktyTxt.text=punkty.ToString();
+
+            // Opcjonalnie, możesz tutaj dodać kod do obsługi dźwięku lub efektów wizualnych.
+            // Na przykład: collision.gameObject.GetComponent<AudioSource>().Play();
+            // Lub: Instantiate(efektZdobytegoPunktu, transform.position, transform.rotation);
+
+            Destroy(collision.gameObject); // Usuń obiekt, z którym zderzył się gracz.
+        }
+    }
+
+
 }
